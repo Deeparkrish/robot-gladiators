a7fec77f7845a7023a3877e7d77a152db156a0ab
Deepa <kosalai17@gmail.com>
2021-05-10 13:38:23 -0700

    Added functions to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("My first Javascript code")
+// this creates a function named "fight"
+function fight(){
+window.alert("The fight has begun !!");
+}
+// Create a function to ask the player for information 
+function input(){
+    var playerName =window.prompt("What is your player's name?");
+    console.log(playerName);
+}
+//fight();
+input();
+
