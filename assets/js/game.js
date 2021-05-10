6b86fe43e55ddc5fe177ebd9a7beeaabf131da96
Deepa <kosalai17@gmail.com>
2021-05-10 14:40:41 -0700

    added  player and enenmy data fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,41 @@
+// Variables to hold  the player data 
+var playerName = window.prompt("What is your player's name?");
+var playerHealth=100;
+var playerAttack= 10;
+
+console.log(playerName,playerHealth,playerAttack);
+
+//Initializing the enemy robot data 
+var enemyName="Roborto"
+var enemyHealth=50;
+var enemyattack= 12;
+
+
 // this creates a function named "fight"
 function fight(){
-window.alert("The fight has begun !!");
+window.alert("Welcome to Robot Gladiators!");
+enemyHealth=enemyHealth-playerAttack;
+console.log (playerName+" attacked "+ enemyName +" . " + enemyName +" now has "+enemyHealth + " points. ");
+if (enemyHealth <=0){
+    console.log(enemyName + "has died.");
 }
-// Create a function to ask the player for information 
-function input(){
-    var playerName =window.prompt("What is your player's name?");
-    console.log(playerName);
+else{
+    window.alert(enemyName+ " still has "+ enemyHealth+ " health points left.");
 }
-//fight();
-input();
+//enemy attacked player 
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+// check player's health
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} 
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+}
+//execute the function
+fight();
+
+
+
 
